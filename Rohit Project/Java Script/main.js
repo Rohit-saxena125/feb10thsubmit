3d0d93b7d40ba185a6902521929fa5785980e37a
RISHABH AGARWAL <73248554+Rish94@users.noreply.github.com>
2023-02-08 20:41:03 +0530

    Add files via upload

---
+++ Rohit Project/Java Script/main.js
@@ -0,0 +1,7 @@
+const  button = document.getElementById('agentsignup');
+
+button.onclick = function (){
+    window.open('../Pages/AgentSignUpPage.html','_self');
+}
+
+// button.style.backgroundColor = "blue";
